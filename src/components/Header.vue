f826a0e5f2fd6f01d640613c16de6e198c9143ba
lijian <mxy_lv@163.com>
2025-05-16 13:13:19 +0800

    初始化

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav>
+        <RouterLink to="/">Smail</RouterLink>
+        <ul>
+            <li>
+                <RouterLink to="/">首页</RouterLink>
+            </li>
+            <li>
+                <RouterLink to="/">首页</RouterLink>
+            </li>
+            <li>
+                <RouterLink to="/">首页</RouterLink>
+            </li>
+            <li>
+                <RouterLink to="/">关于博主</RouterLink>
+            </li>
+            <li>
+                <RouterLink to="/">留言板</RouterLink>
+            </li>
+        </ul>
+    </nav>
+</template>
---
+++ src/views/home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="layui-box">
+        <Header />
+    </div>
+</template>
+<script lang="ts" setup>
+    import Header from '@/components/Header.vue';
+</script>
